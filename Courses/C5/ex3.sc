bdf79e2f701be9fbdf8cb1f81905231c10dea92f
mmswflow-upb <152873071+mmswflow-upb@users.noreply.github.com>
2025-04-03 04:06:20 +0300

    HW 1

--- Courses/C5/ex3.sc
+++ Courses/C5/ex3.sc
@@ -28,6 +28,9 @@ def prod(l : List[Int]) : Int = {
 How can we generalize this code?
  */
 
+/*
+this is actually a right fold
+ */
 def fold1(acc: Int)( op: (Int, Int) => Int)(l: List[Int]) : Int = {
 
   def loop(l_loop: List[Int]) : Int = {
@@ -39,10 +42,53 @@ def fold1(acc: Int)( op: (Int, Int) => Int)(l: List[Int]) : Int = {
 
   loop(l)
 
-
 }
+
+
 
 //Only for some lambda does it work, first underscore is first parameter and second _ is second parameter
 // val closure_sum = fold1(0)((_,_) => _ + _)
 val closure_sum = fold1(0)((x,y) => x + y)
 closure_sum(List(1,2,3,4,5))
+
+
+/*
+Making fold tail-recursive (this is now a left fold)
+
+ */
+
+def tailFold(acc: Int)(op: (Int, Int) => Int)(l: List[Int]): Int = {
+  l match {
+    case Nil => acc
+    case x :: xs => tailFold(op(acc, x))(op)(xs)
+  }
+
+}
+
+/*
+If we have op(x,y) => y::x , this will create a list with y as its new head and x as its tail or the rest of the list
+
+ */
+
+def fold3[T](acc: T)(op: (T, Int) => T)(l: List[Int]): T = {
+
+  l match {
+    case Nil => acc
+    case x :: xs => fold3(op(acc, x))(op)(l)
+  }
+}
+
+val reverse = fold3(Nil)((x: List[Int], y : Int) => y::x)
+
+
+//reverse(List(1,2,3))
+
+List(1,2,3).foldRight(Nil: List[Int])(_ :: _) // return the same list as before
+
+List(1,2,3).foldLeft(Nil: List[Int])((l: List[Int], head: Int) => head :: l) // return the reversed list
+
+List(1,2,3).map(_+2) // returns a list of the same elements but with 2 added to them
+
+List(1,2,3).filter(_%2==0) // returns divisors of 2 only from the list 1,2,3
+
+List(1,2,3).zip(List(5,6,7)) // if theres more elements on one side, it will ignore
---
+++ Homework/HW1/FSets.scala
@@ -0,0 +1,85 @@
+object FSets {
+
+  type Set = Int => Boolean
+
+  def tokenID: Int = 559403
+
+  def member(e: Int)(s: Set): Boolean  = s(e)
+
+  // This defines a set of one element, this characteristic function will check if any given element matches the singleton's sole element
+  def singleton(x: Int): Set = (y: Int) => x == y
+
+
+  def ins(x: Int)(s: Set): Set = (y: Int) => singleton(x)(y) || s(y)
+
+  def fromBounds(start: Int, stop: Int): Set = (y:Int) => {
+    /*
+
+    Non-tail recursive version:
+
+    if(start > stop)  false
+    else  fromBounds(start+1, stop)(y) || singleton(start)(y)
+
+    */
+    def helperFromBounds(i: Int, traversedSet: Set) : Boolean = {
+      if(i > stop) traversedSet(y)
+      else helperFromBounds(i+1, ins(i)(traversedSet))
+    }
+    helperFromBounds(start, y => false)
+  }
+
+  def union (s1: Set, s2: Set): Set = (x: Int) => s1(x) || s2(x)
+
+  def complement(s1: Set): Set = (y: Int) => !s1(y)
+
+  def sumSet(b: Int)(start: Int, stop: Int)(s: Set): Int = {
+    def aux(crt: Int, acc: Int): Int = {
+      if(crt > stop) acc
+      else if(s(crt)) aux(crt+1, acc + crt ) else aux(crt+1, acc)
+    }
+
+    aux(start, b)
+  }
+
+  def foldLeftSet(b:Int)(op: (Int,Int) => Int)(start: Int, stop: Int)(s: Set): Int = {
+    def aux(crt: Int, acc: Int): Int = {
+      if(crt > stop) acc
+      else if(s(crt)) aux(crt+1, op(acc, crt)) else aux(crt+1, acc)
+    }
+    aux(start,b)
+  }
+
+  def foldRightSet(b:Int)(op: (Int,Int) => Int)(start: Int, stop: Int)(s: Set): Int = {
+    if (start > stop) b
+    else if(start == stop) if(s(start)) op(start, b) else b
+    else if(s(start)) op(start, foldRightSet(b)(op)(start+1, stop)(s)) else foldRightSet(b)(op)(start+1, stop)(s)
+
+  }
+
+  def filter(p: Int => Boolean)(s: Set): Set = (y: Int) => s(y) && p(y)
+
+  def partition(p: Int => Boolean)(s: Set): (Set,Set) =  ( (y: Int) => s(y) && p(y), (y: Int) => s(y) && !p(y))
+
+  def forall(cond: Int => Boolean)(start: Int, stop: Int)(s: Set): Boolean = {
+    if(start > stop) true
+    else if(s(start) && !cond(start)) false
+    else forall(cond)(start+1,stop)(s)
+  }
+
+
+  def exists(cond: Int => Boolean)(start: Int, stop: Int)(s: Set): Boolean = {
+    if(start > stop) false
+    else if(s(start) && cond(start)) true
+    else exists(cond)(start+1, stop)(s)
+  }
+
+  def setOfDivByK(k: Int): Set = filter((x: Int) => x % k == 0)(_ => true)
+
+  def moreDivs(k: Int)(start: Int, stop:Int)(s1: Set, s2: Set): Boolean = {
+
+    val increment = (x: Int, y: Int) => x + 1
+    val numOfDivs = (s: Set) => foldLeftSet(0)(increment)(start,stop)(filter(setOfDivByK(k))(s))
+     numOfDivs(s1) > numOfDivs(s2)
+  }
+
+}
